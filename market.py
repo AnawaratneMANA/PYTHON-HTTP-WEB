3b4ae038ff9b292a8eb2df16ac6f1b30518557ee
AnawaratneMANA <it19086408@my.sliit.lk>
2022-01-24 13:20:55 -0500

    Adding a new route.

--- market.py
+++ market.py
@@ -3,4 +3,8 @@ app = Flask(__name__) # built in variable.
 
 @app.route("/")
 def hello_world():
-    return "<p>Changed the Text Live Coding with Debug Mode.</p>"
+    return "<p>Hello World</p>"
+
+@app.route('/about')
+def about_page():
+    return "About Text"
