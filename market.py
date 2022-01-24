e1edb6646913217bf67ff5fc7d966bb7a3d1ee82
AnawaratneMANA <it19086408@my.sliit.lk>
2022-01-24 13:28:53 -0500

    Displaying External HTML files with Render Template.

--- market.py
+++ market.py
@@ -1,10 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__) # built in variable.
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello World</p>"
-
-@app.route('/about/<username>')
-def about_page(username):
-    return f'<h1>This is the dynamic Route {username}</h1>'
+def home_page():
+    return render_template('home.html')
