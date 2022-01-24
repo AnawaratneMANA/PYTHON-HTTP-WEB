27c96d9eae44f223fb1f55fd92bdc035ce91bf44
AnawaratneMANA <it19086408@my.sliit.lk>
2022-01-24 13:17:43 -0500

    Content Changed.

--- market.py
+++ market.py
@@ -3,4 +3,4 @@ app = Flask(__name__) # built in variable.
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Changed the Text Live Coding with Debug Mode.</p>"
