36c12874c6bc6328c05e4eecc1279c92c67cac4e
AnawaratneMANA <it19086408@my.sliit.lk>
2022-01-25 13:13:35 -0500

    Passing set of dictionaries to template.

--- market.py
+++ market.py
@@ -8,4 +8,9 @@ def home_page():
 
 @app.route('/market')
 def market_page():
-    return render_template('market.html', item_name="Phone")
+    items = [
+        {'id': 1, 'name': 'Phone', 'barcode': '45345345989', 'price': 500},
+        {'id': 2, 'name': 'Laptop', 'barcode': '65398541253', 'price': 600},
+        {'id': 3, 'name': 'Keyboard', 'barcode': '98647521654', 'price': 100}
+    ]
+    return render_template('market.html', items=items)
